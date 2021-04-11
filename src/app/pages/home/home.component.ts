5d302dac8d065744b621b598b95274ab89a6388f
Filip <fduretic@mail.com>
2021-04-11 10:41:47 +0200

    design improvements

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -48,6 +48,42 @@ export class HomeComponent implements OnInit {
         label: 'Set Inox 34/10 lonaca',
         description: 'Set lonaca od nehrđajućeg 34/10 inoxa sa dodatnim premazom dna koje se briše jednim potezom krpice.',
         price: 439.99
+      },
+      {
+        id: '2100',
+        label: 'Masažer M-3000 Plus',
+        description: 'Doživite petu dimenziju sa ovim savršenim masažerom koji podržava više načina rada i brzina',
+        price: 999.99
+      },
+      {
+        id: '2101',
+        label: 'Shaker Beast Mode',
+        description: 'Uspostavite autoritet u teretani sa ovim shakerom',
+        price: 99.99
+      },
+      {
+        id: '2102',
+        label: 'Set kalupa za kolače',
+        description: 'Razveselite svoje najmilije sa ovim setom kalupa raznih oblika',
+        price: 83.99
+      },
+      {
+        id: '2103',
+        label: 'Brusač noževa SharpBlade Ultra Pro',
+        description: 'Neka vaši noževi više nikad ne budu tupi uz ovaj profesionalni alat za brušenje noževa',
+        price: 149.99
+      },
+      {
+        id: '2104',
+        label: 'Pakiranje termo čarapa',
+        description: 'Idealno za skijanje, planinarenje, boso hodanje po pločicama i ostale ekstremne sportove',
+        price: 119.99
+      },
+      {
+        id: '2105',
+        label: 'Set Inox 34/10 lonaca',
+        description: 'Set lonaca od nehrđajućeg 34/10 inoxa sa dodatnim premazom dna koje se briše jednim potezom krpice.',
+        price: 439.99
       }
     ];
   }
